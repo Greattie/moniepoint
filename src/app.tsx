de2490e071b0045788339760dd251587812c9408
Greattie <gamubode@gmail.com>
2024-05-28 18:11:02 +0100

    created staging branch

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,11 @@
-
+import { Select } from "antd";
 
 export function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Select />
+      <p>Monie point</p>
     </>
-  )
+  );
 }
