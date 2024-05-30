fb8c3614e2c37a3454631ee88313ab1568298d9f
Greattie <gamubode@gmail.com>
2024-05-30 17:16:03 +0100

    updated navbar styles

--- src/features/Components/Navbar.tsx
+++ src/features/Components/Navbar.tsx
@@ -14,34 +14,34 @@ export const Navbar: React.FC<INavProps> = ({
   });
   return (
     // <div className="fixed bottom-0 flex border border-orange-600">
-    <div className="grid grid-cols-4 bg-white w-[28rem] p-4 bottom-0 rounded-3xl fixed shadow-custom">
+    <div className="flex justify-between bg-white w-[28rem] p-2 bottom-0 rounded-3xl fixed shadow-custom">
       <Icon
         icon="solar:home-2-bold"
         className="cursor-pointer"
-        width="40"
-        height="40"
+        width="25"
+        height="25"
         style={iconStyle(activeComponent === "Homepage")}
         onClick={() => setActiveComponent("Homepage")}
       />
       <Icon
         icon="solar:chart-linear"
-        width="40"
-        height="40"
+        width="25"
+        height="25"
         className="cursor-pointer"
         style={iconStyle(activeComponent === "Skills")}
         onClick={() => setActiveComponent("Skills")}
       />
       <Icon
         icon="octicon:people-16"
-        width="40"
-        height="40"
+        width="25"
+        height="25"
         className="cursor-pointer"
         style={{ color: "#a3a3a3" }}
       />
       <Icon
         icon="solar:settings-outline"
-        width="40"
-        height="40"
+        width="25"
+        height="25"
         className="cursor-pointer"
         style={{ color: "#a3a3a3" }}
       />
